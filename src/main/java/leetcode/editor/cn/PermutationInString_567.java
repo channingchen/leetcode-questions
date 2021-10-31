0100052657e36e36b9cac3c8ccd0c68842ff8016
ChengMing <jslygcm@qq.com>
2021-10-31 17:46:55 +0800

    change git config user.email

---
+++ src/main/java/leetcode/editor/cn/PermutationInString_567.java
@@ -0,0 +1,50 @@
+package leetcode.editor.cn;
+
+//给你两个字符串 s1 和 s2 ，写一个函数来判断 s2 是否包含 s1 的排列。如果是，返回 true ；否则，返回 false 。 
+//
+// 换句话说，s1 的排列之一是 s2 的 子串 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s1 = "ab" s2 = "eidbaooo"
+//输出：true
+//解释：s2 包含 s1 的排列之一 ("ba").
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s1= "ab" s2 = "eidboaoo"
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s1.length, s2.length <= 104 
+// s1 和 s2 仅包含小写字母 
+// 
+// Related Topics 哈希表 双指针 字符串 滑动窗口 
+// 👍 476 👎 0
+
+
+public class PermutationInString_567{
+    public static void main(String[] args){
+        Solution solution = new PermutationInString_567().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
